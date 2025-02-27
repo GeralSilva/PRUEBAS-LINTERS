bbfe18ce3cf963c502e9203111519183cb7f7290
Geraldine <geraldine_basto@outlook.com>
2025-02-26 20:41:46 -0500

    Se agrega rama develop

---
+++ js/modules/main.js
@@ -0,0 +1,5 @@
+import { setupFormHandler } from "./formHandler";
+
+document.addEventListener("DOMContentLoaded", () => {
+    setupFormHandler();
+});
--- modules/main.js
+++
@@ -1,1 +0,0 @@
-import { setupFormHandler } from ""
